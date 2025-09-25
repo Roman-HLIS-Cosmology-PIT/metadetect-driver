acf8791ebd050e8557963905758c9944c0d2fd29
Sidney Mau <sidneymau@gmail.com>
2025-09-25 11:56:18 -0400

    update example test

--- tests/test_IMCOMDriver.py
+++ tests/test_IMCOMDriver.py
@@ -1,77 +1,85 @@
 import os
-import s3fs
-import numpy as np
-import matplotlib.pyplot as plt
-from astropy.visualization import ZScaleInterval, ImageNormalize
+from pathlib import Path
 
-import ngmix
-from metadetect_driver import IMCOMDriver
+# import s3fs
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pyarrow.compute as pc
+
+from pyimcom.analysis import OutImage
+from metadetect_driver import MetaDetectRunner
 
 band = "H158"
-row_ind = 0
+# row_ind = 0
 
-driver = IMCOMDriver(
-    "/hpc/home/yf194/Work/projects/metadetect-driver/config/config_imcom_sci.yaml"
+# # Setup the filesystem
+# fs = s3fs.S3FileSystem(anon=True)
+# # Path to the IMCOM coadds on AWS S3
+# coadd_dir = (
+#     "nasa-irsa-simulations/openuniverse2024/roman/preview/RomanWAS/images/coadds"
+# )
+# band_dir = os.path.join(coadd_dir, band)
+# subdirs = fs.ls(band_dir)
+# img_dir = subdirs[row_ind]
+# imgs = fs.ls(img_dir)
+# image_path = "s3://" + imgs[0]
+
+image_path = str(Path("IMCOM_data") / "images" / band / "output_00_00.fits")
+
+outimage = OutImage(image_path)
+
+mdet_runner = MetaDetectRunner(outimage)
+catalogs, block_indices = mdet_runner.make_catalogs()
+
+res = catalogs[0]
+
+is_primary = pc.field("is_primary") == True
+is_quality = (
+    (pc.field("pgauss_s2n") > 10)
+    & (pc.field("pgauss_T_ratio") > 0.5)
+    & (pc.field("pgauss_flags") == 0)
 )
 
-# Setup the filesystem
-fs = s3fs.S3FileSystem(anon=True)
-# Path to the IMCOM coadds on AWS S3
-coadd_dir = (
-    "nasa-irsa-simulations/openuniverse2024/roman/preview/RomanWAS/images/coadds"
+fig, axs = plt.subplots()
+
+img = outimage.get_coadded_layer(mdet_runner.driver_cfg.get("layer", "SCI"))
+
+norm = mpl.colors.CenteredNorm(0)
+
+cmap = mpl.colors.LinearSegmentedColormap.from_list(
+    "grey_diverging", ["black", "white", "black"]
 )
-band_dir = os.path.join(coadd_dir, band)
-subdirs = fs.ls(band_dir)
-img_dir = subdirs[row_ind]
-imgs = fs.ls(img_dir)
-image = "s3://" + imgs[0]
+axs.imshow(np.arcsinh(img), origin="lower", cmap=cmap, norm=norm)
 
-obs, img = driver.get_obs_imcom(filepath=image, layer=0)
-
-obslist = ngmix.ObsList()
-obslist.append(obs)
-mbobs = ngmix.MultiBandObsList()
-mbobs.append(obslist)
-
-res = driver.run_metadetect(mbobs=mbobs)
-
-print("number of detected objects: {}".format(res["noshear"].size))
-
-buff_mask = (
-    (res["noshear"]["sx_col"] > driver.config["BOUND_SIZE"])
-    & (
-        res["noshear"]["sx_col"]
-        < driver.config["IMG_SIZE"] - driver.config["BOUND_SIZE"]
-    )
-    & (res["noshear"]["sx_row"] > driver.config["BOUND_SIZE"])
-    & (
-        res["noshear"]["sx_row"]
-        < driver.config["IMG_SIZE"] - driver.config["BOUND_SIZE"]
-    )
+axs.scatter(
+    res["noshear"]["sx_col"],
+    res["noshear"]["sx_row"],
+    c="r",
+    marker="x",
+    s=72,
+    label="Detection",
 )
-m_good = (
-    (res["noshear"]["wmom_s2n"] > 10)
-    & (res["noshear"]["wmom_T_ratio"] > 0.5)
-    & (res["noshear"]["wmom_flags"] == 0)
-    & buff_mask
+axs.scatter(
+    res["noshear"].filter(is_primary)["sx_col"],
+    res["noshear"].filter(is_primary)["sx_row"],
+    ec="b",
+    fc="none",
+    marker="s",
+    s=72,
+    label="Primary",
 )
-
-plt.figure(figsize=(10, 10))
-m_img = np.mean(img)
-s_img = np.std(img)
-
-interval = ZScaleInterval()
-vmin, vmax = interval.get_limits(img)
-norm = ImageNormalize(img, interval=interval)
-plt.imshow(img, origin="lower", cmap="viridis", norm=norm)
-
-plt.plot(res["noshear"]["sx_col"], res["noshear"]["sx_row"], "b.", label="All")
-plt.plot(
-    res["noshear"]["sx_col"][m_good],
-    res["noshear"]["sx_row"][m_good],
-    "r.",
-    label="Good",
+axs.scatter(
+    res["noshear"].filter(is_quality)["sx_col"],
+    res["noshear"].filter(is_quality)["sx_row"],
+    ec="m",
+    fc="none",
+    marker="D",
+    s=72,
+    label="Quality",
 )
-plt.colorbar()
-plt.legend()
-plt.savefig("metadetect_imcom_sci_test.png")
+fig.colorbar(mpl.cm.ScalarMappable(norm, cmap), ax=axs)
+axs.legend()
+plt.show()
+
+print(res["noshear"].schema)
