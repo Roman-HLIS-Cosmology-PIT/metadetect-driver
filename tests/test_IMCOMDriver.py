d619c90224480eff8c480cbf94c76a58bbd95850
Arun Kannawadi <arunkannawadi@astro.princeton.edu>
2025-10-27 18:43:32 -0400

    Write test file in pytest framework
    Just make sure that tests run for now.

--- tests/test_IMCOMDriver.py
+++ tests/test_IMCOMDriver.py
@@ -8,71 +8,70 @@ from astropy.visualization import ImageNormalize, ZScaleInterval
 
 from metadetect_driver import IMCOMDriver
 
-band = "H158"
-row_ind = 0
 
-driver = IMCOMDriver(
-    "config/imcom_sci.yaml"
-)
+def test_imcomdriver_pipeline(band="H158", show=False):
+    row_ind = 0
 
-# Setup the filesystem
-fs = s3fs.S3FileSystem(anon=True)
-# Path to the IMCOM coadds on AWS S3
-coadd_dir = (
-    "nasa-irsa-simulations/openuniverse2024/roman/preview/RomanWAS/images/coadds"
-)
-band_dir = os.path.join(coadd_dir, band)
-subdirs = fs.ls(band_dir)
-img_dir = subdirs[row_ind]
-imgs = fs.ls(img_dir)
-image = "s3://" + imgs[0]
-
-obs, img = driver.get_obs_imcom(filepath=image, layer=0)
-
-obslist = ngmix.ObsList()
-obslist.append(obs)
-mbobs = ngmix.MultiBandObsList()
-mbobs.append(obslist)
-
-res = driver.run_metadetect(mbobs=mbobs)
-
-print("number of detected objects: {}".format(res["noshear"].size))
-
-buff_mask = (
-    (res["noshear"]["sx_col"] > driver.config["BOUND_SIZE"])
-    & (
-        res["noshear"]["sx_col"]
-        < driver.config["IMG_SIZE"] - driver.config["BOUND_SIZE"]
+    driver = IMCOMDriver(
+        "config/imcom_sci.yaml"
     )
-    & (res["noshear"]["sx_row"] > driver.config["BOUND_SIZE"])
-    & (
-        res["noshear"]["sx_row"]
-        < driver.config["IMG_SIZE"] - driver.config["BOUND_SIZE"]
+
+    # Setup the filesystem
+    fs = s3fs.S3FileSystem(anon=True)
+    # Path to the IMCOM coadds on AWS S3
+    coadd_dir = (
+        "nasa-irsa-simulations/openuniverse2024/roman/preview/RomanWAS/images/coadds"
     )
-)
-m_good = (
-    (res["noshear"]["wmom_s2n"] > 10)
-    & (res["noshear"]["wmom_T_ratio"] > 0.5)
-    & (res["noshear"]["wmom_flags"] == 0)
-    & buff_mask
-)
+    band_dir = os.path.join(coadd_dir, band)
+    subdirs = fs.ls(band_dir)
+    img_dir = subdirs[row_ind]
+    imgs = fs.ls(img_dir)
+    image = "s3://" + imgs[0]
 
-plt.figure(figsize=(10, 10))
-m_img = np.mean(img)
-s_img = np.std(img)
+    obs, img = driver.get_obs_imcom(filepath=image, layer=0)
 
-interval = ZScaleInterval()
-vmin, vmax = interval.get_limits(img)
-norm = ImageNormalize(img, interval=interval)
-plt.imshow(img, origin="lower", cmap="viridis", norm=norm)
+    obslist = ngmix.ObsList()
+    obslist.append(obs)
+    mbobs = ngmix.MultiBandObsList()
+    mbobs.append(obslist)
 
-plt.plot(res["noshear"]["sx_col"], res["noshear"]["sx_row"], "b.", label="All")
-plt.plot(
-    res["noshear"]["sx_col"][m_good],
-    res["noshear"]["sx_row"][m_good],
-    "r.",
-    label="Good",
-)
-plt.colorbar()
-plt.legend()
-plt.savefig("metadetect_imcom_sci_test.png")
+    res = driver.run_metadetect(mbobs=mbobs)
+
+    print("number of detected objects: {}".format(res["noshear"].size))
+
+    buff_mask = (
+        (res["noshear"]["sx_col"] > driver.config["BOUND_SIZE"])
+        & (
+            res["noshear"]["sx_col"]
+            < driver.config["IMG_SIZE"] - driver.config["BOUND_SIZE"]
+        )
+        & (res["noshear"]["sx_row"] > driver.config["BOUND_SIZE"])
+        & (
+            res["noshear"]["sx_row"]
+            < driver.config["IMG_SIZE"] - driver.config["BOUND_SIZE"]
+        )
+    )
+    m_good = (
+        (res["noshear"]["wmom_s2n"] > 10)
+        & (res["noshear"]["wmom_T_ratio"] > 0.5)
+        & (res["noshear"]["wmom_flags"] == 0)
+        & buff_mask
+    )
+
+    if show:
+        plt.figure(figsize=(10, 10))
+
+        interval = ZScaleInterval()
+        norm = ImageNormalize(img, interval=interval)
+        plt.imshow(img, origin="lower", cmap="viridis", norm=norm)
+
+        plt.plot(res["noshear"]["sx_col"], res["noshear"]["sx_row"], "b.", label="All")
+        plt.plot(
+            res["noshear"]["sx_col"][m_good],
+            res["noshear"]["sx_row"][m_good],
+            "r.",
+            label="Good",
+        )
+        plt.colorbar()
+        plt.legend()
+        plt.savefig("metadetect_imcom_sci_test.png")
