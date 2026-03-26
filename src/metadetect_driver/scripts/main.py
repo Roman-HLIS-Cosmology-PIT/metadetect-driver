d4a75c6e81df1d01f4e7ad68303464b014000c40
Sidney Mau <sidneymau@gmail.com>
2026-03-26 16:46:20 -0400

    need np for rng

--- src/metadetect_driver/scripts/main.py
+++ src/metadetect_driver/scripts/main.py
@@ -7,6 +7,7 @@ import concurrent.futures
 from pathlib import Path
 import logging
 
+import numpy as np
 from pyimcom.analysis import OutImage
 from pyimcom.compress.compressutils import ReadFile
 import pyarrow.parquet as pq
