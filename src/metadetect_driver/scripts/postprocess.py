9b5a1690ede3a03a104c2b1a34a94e4a14897cff
Sidney Mau <sidneymau@gmail.com>
2026-06-02 15:11:11 -0400

    generalize some of the name/field handling of the postprocess script for both esheldon/metadetect and aguinot/metacoadd

--- src/metadetect_driver/scripts/postprocess.py
+++ src/metadetect_driver/scripts/postprocess.py
@@ -1,5 +1,6 @@
 import argparse
 import functools
+import re
 import time
 from pathlib import Path
 
@@ -15,6 +16,10 @@ import pyarrow as pa
 import pyarrow.compute as pc
 import pyarrow.dataset as ds
 import yaml
+
+
+# SourceExtractor names to skip for mag/dered processing
+SKIP_NAMES = {"flux", "flux_err", "flux_radius"}
 
 ROMAN_BAND_KEYS = {
     "W": "W146",
@@ -41,23 +46,35 @@ def _process_batch(batch, bands, mask, dustmap):
     batch = batch.append_column(masked_field, masked)
 
     # could be a few fields dependong on which algos run...
+    mag_names = []
     for name in batch.column_names:
-        if ("flux" in name) and ("flags" not in name):
+        if ("flux" in name) and ("flags" not in name) and (name not in SKIP_NAMES):
             field = batch.field(name)
-            _subarrays = []
-            for i in range(field.type.num_fields):
+            _num_fields = field.type.num_fields
+            if _num_fields > 0:
+                _subarrays = []
+                for i in range(_num_fields):
+                    band = bands[i]
+                    band_key = ROMAN_BAND_KEYS[band]
+                    bandpass = ROMAN_BANDPASSES[band_key]
+                    _data = (
+                        -2.5 * np.log10(pc.list_element(batch[name], i))
+                        + bandpass.zeropoint
+                    )
+                    _subarrays.append(_data)
+                array = pa.array(zip(*_subarrays))
+            else:
+                i = int(re.search(r'_(\d+)$', name).group(1))
                 band = bands[i]
                 band_key = ROMAN_BAND_KEYS[band]
                 bandpass = ROMAN_BANDPASSES[band_key]
-                _data = (
-                    -2.5 * np.log10(pc.list_element(batch[name], i))
-                    + bandpass.zeropoint
-                )
-                _subarrays.append(_data)
-            subarrays = pa.array(zip(*_subarrays))
+                _data = -2.5 * np.log10(batch[name]) + bandpass.zeropoint
+                array = _data
+
             mag_name = name.replace("flux", "mag")
             mag_field = pa.field(mag_name, field.type)
-            batch = batch.append_column(mag_field, subarrays)
+            batch = batch.append_column(mag_field, array)
+            mag_names.append(mag_name)
 
     _coords = SkyCoord(ra, dec, unit="deg")
     _ebv = dustmap.query(_coords).astype(np.float64)
@@ -65,17 +82,21 @@ def _process_batch(batch, bands, mask, dustmap):
     ebv_field = pa.field("ebv", pa.float64())
     batch = batch.append_column(ebv_field, ebv)
 
-    for name in batch.column_names:
-        if ("mag" in name) and ("flags" not in name):
-            field = batch.field(name)
+    for name in mag_names:
+        field = batch.field(name)
+        _num_fields = field.type.num_fields
+        if _num_fields > 0:
             _subarrays = []
-            for i in range(field.type.num_fields):
+            for i in range(_num_fields):
                 _data = pc.add(pc.list_element(batch[name], i), ebv)
                 _subarrays.append(_data)
-            subarrays = pa.array(zip(*_subarrays))
-            dered_name = name + "_dered"
-            dered_field = pa.field(dered_name, field.type)
-            batch = batch.append_column(dered_field, subarrays)
+            array = pa.array(zip(*_subarrays))
+        else:
+            _data = pc.add(batch[name], ebv)
+            array = _data
+        dered_name = name + "_dered"
+        dered_field = pa.field(dered_name, field.type)
+        batch = batch.append_column(dered_field, array)
 
     is_primary_block = batch["is_primary"]
 
@@ -111,31 +132,34 @@ def _process_batch(batch, bands, mask, dustmap):
     batch = batch.append_column(is_primary_mosaic_field, is_primary_mosaic)
     batch = batch.append_column(is_primary_field, is_primary)
 
+    flag_names = []
+    _flagged_columns = []
+    for name in batch.column_names:
+        if "flags" in name:
+            field = batch.field(name)
+            _num_fields = field.type.num_fields
+            if _num_fields > 0:
+                for i in range(_num_fields):
+                    _flagged_columns.append(pc.list_element(batch[name], i))
+            else:
+                _flagged_columns.append(batch[name])
+            flag_names.append(name)
+
     flag_field = pa.field("flagged", pa.bool_())
     _flagged = functools.reduce(
         pc.bit_wise_or,
-        [
-            batch["gauss_flags"],
-            batch["pgauss_flags"],
-            batch["psfrec_flags"],
-            batch["gauss_T_flags"],
-            pc.list_element(batch["pgauss_band_flux_flags"], 0),
-            pc.list_element(batch["pgauss_band_flux_flags"], 1),
-            pc.list_element(batch["pgauss_band_flux_flags"], 2),
-        ],
+        _flagged_columns
     )
     flagged = pc.equal(_flagged, 0)
     batch = batch.append_column(flag_field, flagged)
 
-    batch = batch.drop_columns(
-        [
-            "gauss_flags",
-            "pgauss_flags",
-            "psfrec_flags",
-            "gauss_T_flags",
-            "pgauss_band_flux_flags",
-        ]
-    )
+    # TODO: remove uncompressed flags
+    # drop_columns = []
+    # for name in batch.column_names:
+    #     if "flags" in name:
+    #         drop_columns.append(name)
+
+    # batch = batch.drop_columns(drop_columns)
 
     return batch
 
@@ -149,22 +173,23 @@ def _process_dataset(dataset, bands, mask, dustmap):
     schema = schema.append(masked_field)
 
     # could be a few fields dependong on which algos run...
+    mag_names = []
     for name in schema.names:
-        if ("flux" in name) and ("flags" not in name):
+        if ("flux" in name) and ("flags" not in name) and (name not in SKIP_NAMES):
             field = schema.field(name)
             mag_name = name.replace("flux", "mag")
             mag_field = pa.field(mag_name, field.type)
             schema = schema.append(mag_field)
+            mag_names.append(mag_name)
 
     ebv_field = pa.field("ebv", pa.float64())
     schema = schema.append(ebv_field)
 
-    for name in schema.names:
-        if ("mag" in name) and ("flags" not in name):
-            field = schema.field(name)
-            dered_name = name + "_dered"
-            dered_field = pa.field(dered_name, field.type)
-            schema = schema.append(dered_field)
+    for name in mag_names:
+        field = schema.field(name)
+        dered_name = name + "_dered"
+        dered_field = pa.field(dered_name, field.type)
+        schema = schema.append(dered_field)
 
     index = schema.get_field_index("is_primary")
     schema = schema.remove(index)
@@ -179,15 +204,11 @@ def _process_dataset(dataset, bands, mask, dustmap):
     flag_field = pa.field("flagged", pa.bool_())
     schema = schema.append(flag_field)
 
-    for name in [
-        "gauss_flags",
-        "pgauss_flags",
-        "psfrec_flags",
-        "gauss_T_flags",
-        "pgauss_band_flux_flags",
-    ]:
-        index = schema.get_field_index(name)
-        schema = schema.remove(index)
+    # TODO: remove uncompressed flags
+    # for name in schema.names:
+    #     if "flags" in name:
+    #         index = schema.get_field_index(name)
+    #         schema = schema.remove(index)
 
     def _batch_iterator(dataset, schema, bands, mask, dustmap):
         for batch in dataset.to_batches():
